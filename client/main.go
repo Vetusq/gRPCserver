3254bf9180c2227b649364135184d00dcf9ad666
Dan <grever321@gmail.com>
2024-03-01 10:11:11 +0300

    Final version with eth balance and nonce client output

--- client/main.go
+++ client/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math"
+	"math/big"
 
 	pb "github.com/Vetusq/gRPCserver/invoicer"
 	"google.golang.org/grpc"
@@ -19,7 +21,7 @@ func main() {
 	defer conn.Close()
 	client := pb.NewGreeterClient(conn)
 
-	walletAddress := "0x84d34f4f83a87596cd3fb6887cff8f17bf5a7b83"
+	walletAddress := "0xef89e95c889f349f8ae3c226d87c94a96f6a9bfc"
 
 	req := &pb.RequestWalletInfo{
 		Wallet: walletAddress,
@@ -27,9 +29,14 @@ func main() {
 
 	resp, err := client.GetBalance(context.Background(), req)
 	if err != nil {
-		log.Fatalf("Error calling GetBalance: %v", err)
+		log.Fatalf("Error calling GetBalance: %s", err)
 	}
 
-	fmt.Printf("Balance: %d\n WEI\n", resp.Balance)
+	ethBalanceBigInt := big.NewInt(resp.Balance)
+	fbalance := new(big.Float)
+	fbalance.SetString(ethBalanceBigInt.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+
+	fmt.Printf("Balance: %.5f ETH\n", ethValue)
 	fmt.Printf("Nonce: %d\n", resp.Nonce)
 }
